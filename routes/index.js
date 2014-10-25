40eadd53b1d0937f79ce2f952de350252165536d
Cesar Vargas <cesarvargas00@gmail.com>
2014-10-24 20:15:38 -0400

    routes index new

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var blu;
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/bla', function(req, res){
-    res.json({bla:'blu'});
+    res.json({bla:blu});
 });
 
 router.get('/subscribe', function(req, res){
     console.log(req.query["hub.challenge"]);
     res.send(req.query["hub.challenge"].toString());
+});
+
+router.post('/subscribe', function(req, res){
+    console.log(req.query["hub.challenge"]);
+    blu = req.body;
+    res.send(200);
 });
 
 module.exports = router;
